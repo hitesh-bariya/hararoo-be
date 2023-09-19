9030c8a483f8392bf74b9a5ffab5b9d6b6bb108f
hitesh.bariya <hiteshbariya310@gmail.com>
2023-09-20 00:03:12 +0530

    abcd

--- src/main/java/com/hararoobe/hararoo/controller/AdminController.java
+++ src/main/java/com/hararoobe/hararoo/controller/AdminController.java
@@ -52,6 +52,7 @@ public class AdminController {
 	public ResponseVO<?> getAllJobs(@RequestParam(defaultValue = "0") Integer pageNo,
 			@RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "jobId") String sortBy) {
 		log.info("Calling Contoller for getAllJobs mrthod");
+		
 		return ResponseVO.builder().status(201).body(jobService.getAllJobData(pageNo, pageSize, sortBy)).build();
 	}
 
